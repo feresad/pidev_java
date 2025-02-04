d3ebb6004fdebb17855c67430a3b5335379a4fae
Adouani Feres <feresadouani10@gmail.com>
2025-02-04 23:05:24 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.pidev_java {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.pidev_java to javafx.fxml;
+    exports org.example.pidev_java;
+}
